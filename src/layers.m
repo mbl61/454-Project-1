63703fc172a7ac15d37bb0fda52c0c330affe3ec
ｍｃｌ <mbl6031@psu.edu>
2026-09-24 14:10:10 -0400

    Fixed empty filterbank problem

--- src/layers.m
+++ src/layers.m
@@ -25,14 +25,14 @@ outarray = inarray; % initialize output array
 outarray = apply_imnormalize(outarray); % normalize input image
 
 for i = 1:3
-    outarray = apply_convolve(outarray, filterbanks{2 * i}, biasvectors{2 * i}); % input N*M*D1, filterbank N*M*D1*D2, biasvals length D2, output N*M*D2
+    outarray = apply_convolve(outarray, filterbanks{5 * i - 3}, biasvectors{5 * i - 3}); % input N*M*D1, filterbank N*M*D1*D2, biasvals length D2, output N*M*D2
     outarray = apply_relu(outarray); % apply ReLU
-    outarray = apply_convolve(outarray, filterbanks{2 * i + 1}, biasvectors{2 * i + 1}); % input N*M*D1, filterbank N*M*D1*D2, biasvals length D2, output N*M*D2
+    outarray = apply_convolve(outarray, filterbanks{5 * i - 1}, biasvectors{5 * i - 1}); % input N*M*D1, filterbank N*M*D1*D2, biasvals length D2, output N*M*D2
     outarray = apply_relu(outarray); % apply ReLU
     outarray = apply_maxpool(outarray); % halve the size of the image
 end
 
-outarray = apply_fullconnect(outarray, filterbanks{7}, biasvectors{7}); % fully connect layer
+outarray = apply_fullconnect(outarray, filterbanks{17}, biasvectors{17}); % fully connect layer
 
 outarray = apply_softmax(outarray); % output 1*1*10
 
